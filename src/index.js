175dfeafc2f0d65dd44be28ae6002be9e10cfae0
igor-mlv <malov7704@gmail.com>
2023-11-22 15:20:27 -0500

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from "./components/App";
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById("root")
-);
+const root = document.getElementById('root');
+const reactRoot = createRoot(root);
+
+reactRoot.render(<App />);
